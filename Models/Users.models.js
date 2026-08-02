d2b49b75729794023cc972ab62220b5c904f14f4
Rain <tunmiodus@gmail.com>
2026-08-02 08:47:49 +0100

    Supermarket: First upload

---
+++ Models/Users.models.js
@@ -0,0 +1,44 @@
+
+const mongoose = require('mongoose');
+const bcrypt = require ('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: { 
+      type: String, 
+      required: true 
+  },
+  email: { 
+      type: String, 
+      required: true,
+      unique: true
+  },
+  password: { 
+      type: String, 
+      required: true 
+  },
+  sex: { 
+      type: String, 
+      required: true 
+  },
+  hasAtmCard: {
+    type: Boolean,
+    default: false
+  },
+  phone: { 
+      type: String, 
+      required: true 
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user'
+  },
+}, {
+  timestamps: true // Date user is created and updated 
+
+});
+
+//Create Model from Schema
+const User = mongoose.model('User', userSchema);
+
+module.exports = User; 
